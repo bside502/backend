b386b2cf73c7cbb19c0b4674f4d70eb688466bbc
tmddus2123 <tmddus2123@gmail.com>
2025-02-23 13:39:35 +0900

    project init

---
+++ src/main/java/com/example/redaeri/RedaeriApplication.java
@@ -0,0 +1,13 @@
+package com.example.redaeri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedaeriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedaeriApplication.class, args);
+	}
+
+}
